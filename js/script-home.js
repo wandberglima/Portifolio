651d65391e09eb18d60b1974a2a66b8259cea7b6
Wandberg de Lima <wandberg@gmail.com>
2022-11-06 02:17:02 -0300

    ajustes de index

--- js/script-home.js
+++ js/script-home.js
@@ -1,11 +1,6 @@
-var paginaAtual = document.getElementById("select").value;
-var mensagem = document.getElementById("mensagem");
-var passo = document.getElementById("passo");
-var qualMetodo = true;
+const procura = document.getElementById('procura');
+    
+    procura.addEventListener('click', () => {
+        console.log('clicando');
 
-function mudaPagina() {
-  const pagina = document.getElementById("select").value;
-  if (pagina !== paginaAtual) {
-    location.href = "index2.html";
-  }
-}
+    });
